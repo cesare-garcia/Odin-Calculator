2312a682667d06757d2aa685c95d108d4a1508d9
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-02-06 01:18:34 -0600

    Add keyboard functionality for operator and equal functions

--- script.js
+++ script.js
@@ -12,25 +12,144 @@ let firstNum = '';
 let operator = '';
 let secondNum = '';
 
+// Keyboard functionality Start
+
 window.addEventListener('keydown', (e) => {
-    console.log(e.key);
-    if ( selectedInputs[0] == undefined ) {
-        if ( firstNum.length < 20 ) {
-            firstNum += e.key;
-            screen.textContent = `${firstNum}`;
-        } else {
-            screen.textContent = `ERROR, string too long.`
+    
+    console.log(e.key)
+
+    if ( e.key == 0 || e.key == 1 || e.key == 2 || e.key == 3 || e.key == 4 || e.key == 5 || e.key == 6 || e.key == 7 || e.key == 8 || e.key == 9) {
+        if ( selectedInputs[0] == undefined ) {
+            if ( firstNum.length < 20 ) {
+                firstNum += e.key;
+                screen.textContent = `${firstNum}`;
+            } else {
+                screen.textContent = `ERROR, string too long.`
+            }
+        } else if ( selectedInputs[0] != undefined && selectedInputs[1] != undefined) {
+            if ( secondNum.length < 20 ) {
+                secondNum += e.key;
+                screen.textContent = `${secondNum}`;
+            } else {
+                screen.textContent = `ERROR, string too long.`
+            }
         }
-    } else if ( selectedInputs[0] != undefined && selectedInputs[1] != undefined) {
-        if ( secondNum.length < 20 ) {
-            secondNum += e.key;
-            screen.textContent = `${secondNum}`;
+    } else if ( e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/') {
+        if ( firstNum == '' && e.key == '+' ) {
+            resetCalc();
+        } else if ( firstNum == '' && e.key == '-' ) {
+            resetCalc();
+        } else if ( firstNum == '' && e.key == '*' ) {
+            resetCalc();
+        } else if ( firstNum == '' && e.key == '/' ) {
+            resetCalc();
+        } else if ( selectedInputs.length == 2 ) {
+            decimal_button.disabled = false;
+            selectedInputs.push(secondNum);
+            let result = operate(selectedInputs[0], selectedInputs[1], selectedInputs[2]);
+            screen.textContent = result;
+            firstNum = result;
+            operator = e.key;
+            secondNum = '';
+            selectedInputs = [];
+            selectedInputs.push(result);
+            selectedInputs.push(operator);
         } else {
-            screen.textContent = `ERROR, string too long.`
+            operator = e.key;
+            if ( selectedInputs[0] != undefined && selectedInputs[1] == undefined ) {
+                selectedInputs.push(operator);
+                decimal_button.disabled = false;
+            } else if ( selectedInputs[0] == undefined && selectedInputs[1] == undefined ) {
+                selectedInputs.push(firstNum);
+                selectedInputs.push(operator);
+                decimal_button.disabled = false;
+            }
         }
+    } else if ( e.key == '=' ) {
+        if ( selectedInputs[0] == undefined && firstNum == '' ) {
+            resetCalc();
+        } else {
+            if ( selectedInputs[2] == undefined ) {
+                selectedInputs.push(secondNum);
+            }
+            
+            let result = operate(selectedInputs[0], selectedInputs[1], selectedInputs[2]);
+            let resultString = result.toString();
+    
+            if ( resultString.length < 20 ) {
+                screen.textContent = result;
+                firstNum = result;
+                operator = '';
+                secondNum = '';
+                selectedInputs = [];
+                selectedInputs.push(result);
+                decimal_button.disabled = true;
+            } else {
+                screen.textContent = `ERROR, string too long.`
+            }
+        }
+    } else if ( ) {
+
     }
+
+
 });
 
+// backspace
+
+
+// if ( selectedInputs[0] == undefined ) {
+//     resetCalc();
+//     decimal_button.disabled = false;
+// } else if ( selectedInputs[0] != undefined && selectedInputs[1] != undefined ) {
+//     selectedInputs.pop();
+//     secondNum = '';
+//     screen.textContent = selectedInputs[0];
+// } else if ( selectedInputs[0] != undefined ) {
+//     resetCalc();
+//     decimal_button.disabled = false;
+// }
+
+
+// clear
+
+// resetCalc();
+// decimal_button.disabled = false;
+
+
+// negative
+
+// if ( selectedInputs[0] != undefined && selectedInputs[1] == undefined ) {
+//     selectedInputs.shift();
+//     firstNum *= -1;
+//     selectedInputs.push(firstNum);
+//     screen.textContent = firstNum;
+// } else if ( selectedInputs[0] == undefined ) {
+//     parseInt(firstNum);
+//     firstNum *= -1;
+//     screen.textContent = firstNum;
+// } else if ( selectedInputs[0] != undefined && selectedInputs[1] != undefined ) {
+//     parseInt(secondNum);
+//     secondNum *= -1;
+//     console.log(secondNum);
+//     screen.textContent = secondNum;
+// }
+
+
+// decimal
+
+// if ( selectedInputs[0] == undefined ) {
+//     firstNum += '.';
+//     screen.textContent = firstNum;
+//     decimal_button.disabled = true;
+// } else if ( selectedInputs[0] != undefined && selectedInputs[1] != undefined ) {
+//     secondNum += '.';
+//     screen.textContent = secondNum;
+//     decimal_button.disabled = true;
+// }
+
+
+// Mouse functionality start
 // Eventually add scientific notation to account for extremely long numbers
 
 digit_buttons.forEach(digit_button => {
@@ -124,7 +243,6 @@ clear_button.addEventListener('click', (e) => {
 });
 
 negative.addEventListener('click', (e) => {
-    console.log(e.target);
     if ( selectedInputs[0] != undefined && selectedInputs[1] == undefined ) {
         selectedInputs.shift();
         firstNum *= -1;
@@ -168,7 +286,6 @@ decimal_button.addEventListener('click', (e) => {
         screen.textContent = secondNum;
         decimal_button.disabled = true;
     }
-
 });
 
 function operate(firstNum, operator, secondNum) {

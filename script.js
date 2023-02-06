715a18e887fc97dd81f5599bffb5fd37dd89098e
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-02-06 02:01:47 -0600

    delete console.log

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ let secondNum = '';
 
 window.addEventListener('keydown', (e) => {
     
-    console.log(e.key)
-
     if ( e.key == 0 || e.key == 1 || e.key == 2 || e.key == 3 || e.key == 4 || e.key == 5 || e.key == 6 || e.key == 7 || e.key == 8 || e.key == 9) {
         if ( selectedInputs[0] == undefined ) {
             if ( firstNum.length < 20 ) {
